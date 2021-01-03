7042af6aec6c61b62fd0022addf4b2e549d191a3
iBars <admin@only-minecraft.ru>
2021-01-03 05:12:35 +0300

    Fixed indentation

--- src/main/java/by/notcz/aromian/controllers/ConverterController.java
+++ src/main/java/by/notcz/aromian/controllers/ConverterController.java
@@ -18,78 +18,75 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 public class ConverterController {
 
 	@CrossOrigin
-	@GetMapping(path = "/convert", produces=MediaType.APPLICATION_JSON_VALUE)
-	public ObjectNode convert(
-		@RequestParam(name = "number", required = false) String request_number,
-		@RequestParam(name = "direction", required = false) String request_direction
-	) {
+	@GetMapping(path = "/convert", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ObjectNode convert(@RequestParam(name = "number", required = false) String request_number, @RequestParam(name = "direction", required = false) String request_direction) {
 		ObjectMapper mapper = new ObjectMapper();
 		ObjectNode response = mapper.createObjectNode();
-	    ObjectNode response_data = mapper.createObjectNode();
+		ObjectNode response_data = mapper.createObjectNode();
 
-	    response.put("success", false);
+		response.put("success", false);
 
-	    if (request_number != null) {
-	    	switch (request_direction) {
-	    		case "roman2arabic":
-			    	Pattern p = Pattern.compile("[IVXLCDM]+");
-			    	Matcher m = p.matcher(request_number);
-			    	if (m.matches()) {
-			    		response_data.put("result", romanToInteger(request_number));
-			    		response.put("success", true);
-			    	} else {
-			    		response.put("message", "Number to convert is not a roman");
-			    	}
+		if (request_number != null) {
+			switch (request_direction) {
+			case "roman2arabic":
+				Pattern p = Pattern.compile("[IVXLCDM]+");
+				Matcher m = p.matcher(request_number);
+				if (m.matches()) {
+					response_data.put("result", romanToInteger(request_number));
+					response.put("success", true);
+				} else {
+					response.put("message", "Number to convert is not a roman");
+				}
 
-			    	break;
+				break;
 
-	    		case "arabic2roman":
-	    	    	try {
-	    	    		int number = Integer.parseInt(request_number);
-	    	    		response_data.put("result", integerToRoman(number));
-	    	    		response.put("success", true);
-	    	    	} catch (NumberFormatException e) {
-	    	    		response.put("message", "Number to convert is not an integer arabic");
-	    	    	}
+			case "arabic2roman":
+				try {
+					int number = Integer.parseInt(request_number);
+					response_data.put("result", integerToRoman(number));
+					response.put("success", true);
+				} catch (NumberFormatException e) {
+					response.put("message", "Number to convert is not an integer arabic");
+				}
 
-	    			break;
+				break;
 
-	    		default:
-	    			response.put("message", "Direction is not specified");
-	    	}
-	    } else {
-	    	response.put("message", "Number to convert is not specified");
-	    }
+			default:
+				response.put("message", "Direction is not specified");
+			}
+		} else {
+			response.put("message", "Number to convert is not specified");
+		}
 
-    	response.put("data", response_data);
-	    return response;
+		response.put("data", response_data);
+		return response;
 	}
 
 
-    public static int romanToInteger(String roman) {
-    	Map<Character, Integer> values = new LinkedHashMap<>();
-    	values.put('I', 1);
-    	values.put('V', 5);
-    	values.put('X', 10);
-    	values.put('L', 50);
-    	values.put('C', 100);
-    	values.put('D', 500);
-    	values.put('M', 1000);
+	public static int romanToInteger(String roman) {
+		Map<Character, Integer> values = new LinkedHashMap<>();
+		values.put('I', 1);
+		values.put('V', 5);
+		values.put('X', 10);
+		values.put('L', 50);
+		values.put('C', 100);
+		values.put('D', 500);
+		values.put('M', 1000);
 
-    	int number = 0;
-    	for (int i = 0; i < roman.length(); i++) {
-    		if (i + 1 == roman.length() || values.get(roman.charAt(i)) >= values.get(roman.charAt(i + 1))) {
-    			number += values.get(roman.charAt(i));
-    		} else {
-    			number -= values.get(roman.charAt(i));
-    		}
-    	}
+		int number = 0;
+		for (int i = 0; i < roman.length(); i++) {
+			if (i + 1 == roman.length() || values.get(roman.charAt(i)) >= values.get(roman.charAt(i + 1))) {
+				number += values.get(roman.charAt(i));
+			} else {
+				number -= values.get(roman.charAt(i));
+			}
+		}
 
-    	return number;
-    }
+		return number;
+	}
 
 
-    public static String integerToRoman(int num) {
+	public static String integerToRoman(int num) {
 		int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 		String[] romanLiterals = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
@@ -102,7 +99,7 @@ public class ConverterController {
 			}
 		}
 
-        return roman.toString();
-    }
+		return roman.toString();
+	}
 
 }
